0fadfcfba74eaa4c393f5adcb080f5aef13c5ddb
Dylan <dylan.cole5652@gmail.com>
2015-04-20 14:40:09 -0600

    added Tree.cpp with empty functions

---
+++ Tree.cpp
@@ -0,0 +1,109 @@
+#include "Tree.h"
+#include <vector>
+#include <string>
+using namespace std;
+
+Tree::Tree()
+{
+    //ctor
+}
+
+Tree::~Tree()
+{
+    //dtor
+}
+
+TreeNode* Tree::searchTree(TreeNode* node, string title, string system, string genre)
+{
+
+}
+
+void Tree::addNode(string title, string type, int quantity, int rating, vector<string> systems)
+{
+
+}
+
+void Tree::printInventory()
+{
+
+}
+
+void Tree::printInventory(TreeNode* node)
+{
+
+}
+
+void Tree::printGenreInventory(string type)
+{
+
+}
+
+void Tree::printGenreInventory(TreeNode* node)
+{
+
+}
+
+void Tree::printConsolInventory(string type)
+{
+
+}
+
+void Tree::printConsolInventory(TreeNode* node)
+{
+
+}
+
+int Tree::countNodes()
+{
+        return -1;
+}
+
+int Tree::countNodes(TreeNode* node)
+{
+        return -1;
+}
+
+void Tree::deleteNode(string title)
+{
+
+}
+
+void Tree::swapNode(TreeNode* oldnode, TreeNode* newnode)
+{
+
+}
+
+TreeNode* Tree::findMin(TreeNode* node)
+{
+
+}
+
+void Tree::findGame(string title)
+{
+
+}
+
+void Tree::rentGame(string title)
+{
+
+}
+
+void Tree::returnGame(string title)
+{
+
+}
+
+void Tree::tradeGame(string title)
+{
+
+}
+
+void Tree::recommendGame(int rating, string type, vector<string> system)
+{
+
+}
+
+void Tree::DeleteAll(TreeNode* node)
+{
+
+}
